3055609d146f11093915d109f7d927ff1cb89f1f
Hackhim <belloharyourmidey@gmail.com>
2025-03-13 04:36:33 -0700

    commented what was there

--- template-literals.js
+++ template-literals.js
@@ -2,11 +2,11 @@
 
 // Try merging them all into a single array using the spread operator!
 
-const fruits = ["apple", "banana", "cherry"];
-const vegetables = ["carrot", "broccoli", "spinach"];
-const grains = ["rice", "quinoa", "oats"];
-const proteins = ["chicken", "fish", "tofu"];
+// const fruits = ["apple", "banana", "cherry"];
+// const vegetables = ["carrot", "broccoli", "spinach"];
+// const grains = ["rice", "quinoa", "oats"];
+// const proteins = ["chicken", "fish", "tofu"];
 
-//merge the four arrays
-const mergingArray = [...fruits,...vegetables,...grains,...proteins]
-console.log(mergingArray)
+// //merge the four arrays
+// const mergingArray = [...fruits,...vegetables,...grains,...proteins]
+// console.log(mergingArray)
