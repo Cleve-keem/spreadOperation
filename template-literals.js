f362be82f49bc2d2a2fe9b160588e943c773290c
Hackhim <belloharyourmidey@gmail.com>
2025-03-13 03:50:03 -0700

    created 4 array questions to merge

---
+++ template-literals.js
@@ -0,0 +1,9 @@
+
+
+// Try merging them all into a single array using the spread operator!
+
+const fruits = ["apple", "banana", "cherry"];
+const vegetables = ["carrot", "broccoli", "spinach"];
+const grains = ["rice", "quinoa", "oats"];
+const proteins = ["chicken", "fish", "tofu"];
+
